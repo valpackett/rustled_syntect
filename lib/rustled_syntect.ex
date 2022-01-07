57f3f6cf138c72fb2f501650c5db158cc3ce4f1e
Brian Romanko <hello@bromanko.com>
2022-01-07 07:46:22 -0800

    Fixing documentation

--- lib/rustled_syntect.ex
+++ lib/rustled_syntect.ex
@@ -13,8 +13,13 @@ defmodule RustledSyntect do
     Nif.new_syntax_set(extra_syntaxes_path)
   end
 
+  @spec hilite_stream(Enumerable.t(), [{:lang, String.Chars.t()}]) :: Enumerable.t()
+  def hilite_stream(stream, lang: lang) do
+    hilite_stream(stream, lang: lang, syntax_set: RustledSyntect.Nif.new_syntax_set())
+  end
+
   @doc ~S'''
-  Syntax highlight an enumerable/stream of lines, producing an iolist.
+  Syntax hilight an enumerable/stream of lines producing an iolist.
 
       iex> Enum.join(RustledSyntect.hilite_stream(["(0..69).each do |x|", "  puts x", "end"], lang: "Ruby"), "") <> "\n"
       """
@@ -33,14 +38,7 @@ defmodule RustledSyntect do
         ["</span>"]
       ]
 
-  '''
-  @spec hilite_stream(Enumerable.t(), [{:lang, String.Chars.t()}]) :: Enumerable.t()
-  def hilite_stream(stream, lang: lang) do
-    hilite_stream(stream, lang: lang, syntax_set: RustledSyntect.Nif.new_syntax_set())
-  end
-
-  @doc ~S'''
-  Syntax hilight an enumerable/stream of lines with a provided syntax set, producing an iolist.
+  You may optionally specify a folder of syntax sets to be added to the default set.
 
       # Assuming the priv/packages directory includes an Elixir.sublime-syntax syntax set:
 
